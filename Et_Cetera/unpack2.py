f3d47698a376ca0779b26343b3ac390dad726f9d
ebornm21 <115953763+ebornm21@users.noreply.github.com>
2023-08-04 12:35:10 -0500

    Delete unpack2.py

--- Et_Cetera/unpack2.py
+++
@@ -1,9 +0,0 @@
-#A program that returns the total value of a given number of coins, as determined by the Harry Potter universe.
-
-def total(galleons, sickles, knuts):
-    return (galleons * 17 + sickles) * 29 + knuts
-
-
-coins = {"galleons": 100, "sickles": 50, "knuts": 25}
-
-print(total(**coins), "Knuts")
