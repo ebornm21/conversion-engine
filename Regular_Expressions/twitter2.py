8b3fd11e62207522409aaf9b7d3bd0c5f6c913f3
ebornm21 <115953763+ebornm21@users.noreply.github.com>
2023-08-04 12:30:37 -0500

    Delete twitter2.py

--- Regular_Expressions/twitter2.py
+++
@@ -1,8 +0,0 @@
-#A program that will parse and return a user's username when given a link to their profile.
-
-import re
-
-url = input("URL: ").strip()
-
-if matches := re.search(r"^https?://(?:www\.)?twitter\.com/([a-z0-9_]+)", url, re.IGNORECASE):
-    print(f"Username:", matches.group(1))
