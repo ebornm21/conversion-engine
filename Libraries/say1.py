d1ef59c94b46f1e15556b79d6cb86385b40a8fe6
ebornm21 <115953763+ebornm21@users.noreply.github.com>
2023-08-04 11:56:13 -0500

    Delete say1.py

--- Libraries/say1.py
+++
@@ -1,8 +0,0 @@
-#A simple program using both the "cowsay" library and the "sys" library and illustrating how they can work together.
-
-import cowsay
-import sys
-
-if len(sys.argv) == 2:
-    cowsay.cow("hello, " + sys.argv[1])
-    cowsay.trex("hello, " + sys.argv[1])
