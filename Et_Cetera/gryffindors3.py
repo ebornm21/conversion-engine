a2b1f008b4a11c29f9b4aee88b4506c4c68b35c2
ebornm21 <115953763+ebornm21@users.noreply.github.com>
2023-08-04 12:33:59 -0500

    Delete gryffindors3.py

--- Et_Cetera/gryffindors3.py
+++
@@ -1,7 +0,0 @@
-#A program that takes a given list and adds them to a new list while appending additional information to each entry.
-
-students = ["Hermione", "Harry", "Ron"]
-
-gryffindors = [{"name": student, "house": "Gryffindor"} for student in students]
-
-print(gryffindors)
