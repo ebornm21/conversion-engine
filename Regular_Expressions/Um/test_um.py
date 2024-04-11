1efbf773291ef4f713e3832d9c8bfbb322e4ce9f
ebornm21 <115953763+ebornm21@users.noreply.github.com>
2024-04-11 12:00:49 -0500

    Delete Regular_Expressions directory

--- Regular_Expressions/Um/test_um.py
+++
@@ -1,9 +0,0 @@
-#Unit tests for um.py
-
-from um import count
-
-def test_count():
-    assert count("um") == 1
-    assert count("um?") == 1
-    assert count("Um, thanks for the album.") == 1
-    assert count("Um, thanks, um...") == 2
