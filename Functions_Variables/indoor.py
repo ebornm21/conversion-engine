596715d9ffb568fd9a464a7ce9ffa4b994195982
ebornm21 <115953763+ebornm21@users.noreply.github.com>
2024-04-11 12:00:08 -0500

    Delete Functions_Variables directory

--- Functions_Variables/indoor.py
+++
@@ -1,7 +0,0 @@
-#A simple program that can change an input to lowercased.
-
-userinput = input("")
-
-userinput = userinput.lower()
-
-print(userinput)
--- Functions_Variables/playback.py
+++
@@ -1,7 +0,0 @@
-#A simple program that will insert ellipses between words, effectively simulating the sentence slowing down.
-
-userinput = input("")
-
-userinput = userinput.replace(" ", "...")
-
-print(userinput)
